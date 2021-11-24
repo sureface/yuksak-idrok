54a9d591eacc91cef18086eb789839a44b2da395
sureface <jasurdjuraev2002@gmail.com>
2021-11-24 18:08:02 +0300

    tailwind css connected to project

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <divc className="container mx-auto">
+            <h1 className="text-3xl text-red-500">asdasdasd</h1>
+        </divc>
+    );
+};
+
+export default Home;
