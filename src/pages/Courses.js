b0c896c21189bb0f13a7ab7cb440e028f4a786ca
sureface <jasurdjuraev2002@gmail.com>
2021-12-03 18:25:48 +0300

    final successed lending page

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Courses = () => {
     return (
         <div>
-            <h1>Courses</h1>
+            <h1 className="text-blue-600">Courses</h1>
         </div>
     );
 };
